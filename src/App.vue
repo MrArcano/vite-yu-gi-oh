fb2ca65a5d863a4385c2c79e6ae81461cf48ea27
MrArcano <salvatore-dario@hotmail.it>
2023-10-12 14:29:18 +0200

    [init]

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+ export default {
+  name: "App"
+ }
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>Ciao</h1>
+  </div>
+
+</template>
+
+<style lang="scss">
+ @use "./scss/main.scss"
+</style>
