95536e22254a9d1f010d5902268f73e641e26f21
MrArcano <salvatore-dario@hotmail.it>
2023-10-12 16:20:00 +0200

    [add]: search select

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,10 +5,19 @@ export default {
 </script>
 
 <template>
-  
+  <div>
+    <select class="form-select" aria-label="Default select example">
+      <option selected>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+  </div>
 </template>
 
 
-<style>
-
+<style lang="scss" scoped>
+  div{
+    margin-bottom: 32px;
+  }
 </style>
